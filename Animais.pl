a2865b52891b9ad55fa53a9cb3b40b6fb4d1e5d2
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2018-10-13 17:45:00 -0300

    Padronizando perguntas
    Signed-off-by: Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
    Co-Authored-by: Lucas Siqueira <lucassiqzro@gmail.com>
    Co-Authored-by: Bruno Dantas <oliveiradantas96@gmail.com>

--- Animais.pl
+++ Animais.pl
@@ -1,26 +1,26 @@
-animal()  :- is_true('tem pelo'), is_true('tem glandulas mamarias'), mamiferos().
-animal()  :- is_true('tem pele grossa e escamosa'), is_true('vive em ambiente terrestre'), repteis().
-animal()  :- is_true('tem bico'), is_true('tem pena'), aves().
-animal()  :- is_true('Vive toda a sua vida dentro da água'), peixes().
+animal()  :- is_true('Tem pelo'), is_true('Tem glandulas mamarias'), mamiferos().
+animal()  :- is_true('Tem pele grossa e escamosa'), is_true('Vive em ambiente terrestre'), repteis().
+animal()  :- is_true('Tem bico'), is_true('Tem pena'), aves().
+animal()  :- is_true('É um animal aquático'), peixes().
 animal()  :- is_true('Tem pele úmida'), is_true('Vivem uma parte de sua vida na água e uma parte na terra'), anfibios().
 
 is_true(Q) :-
         format("~w?\n", [Q]),
         read(sim).
 
-mamiferos() :- is_true('é menor que uma maquina de lavar roupas'), mamiferos_menores().
+mamiferos() :- is_true('É menor que uma maquina de lavar roupas'), mamiferos_menores().
 mamiferos() :- is_true('É conhecido como rei da floresta'), is_true('Tem juba'), write('O animal que vc está pensando é: Leão.').
 mamiferos() :- is_true('Tem um pescoço muito grande'), is_true('Um restaurante famoso utiliza esse animal como sua logomarca'), write('O animal que vc está pensando é: Girrafa').
 mamiferos() :- is_true('Os humanos comsumem amplamente sua carne e leite'), is_true('Faz mu'), write('O animal que vc está pensando é: Vaca').
 mamiferos() :- is_true('O animal relincha'), is_true('É amplamente usado para montaria'), write('O animal que vc está pensando é: Cavalo').
-mamiferos() :- is_true('É um primata'), is_true('Gosta de bananae'), write('O animal que vc está pensando é: Gorila').
+mamiferos() :- is_true('É um primata'), is_true('Gosta de banana'), write('O animal que vc está pensando é: Gorila').
 mamiferos() :- is_true('Tem tromba'), is_true('É o personagem principal do filme dumbo'), write('O animal que vc está pensando é: Elefante').
 mamiferos() :- is_true('Esse animal hiberna'), is_true('É o personagem principal do filme zé colmeia'), write('O animal que vc está pensando é: Urso').
 mamiferos() :- is_true('Esse animal anda pulando'), is_true('São conhecidos como boxeadores, pela forma como lutam'), write('O animal que vc está pensando é: Canguru').
 mamiferos() :- write('Parabéns, você me venceu!! Até a próxima.'), halt(0).
 
-mamiferos_menores() :- is_true('é conhecido como o melhor amigo do homem'), is_true('late'), write('O animal que vc está pensando é: Cachorro.').
-mamiferos_menores() :- is_true('mia'), is_true('quando pula, cai em pé'), write('O animal que vc está pensando é: Gato.').
+mamiferos_menores() :- is_true('É conhecido como o melhor amigo do homem'), is_true('Esse animal late'), write('O animal que vc está pensando é: Cachorro.').
+mamiferos_menores() :- is_true('Esse animal mia'), is_true('Quando pula, cai em pé'), write('O animal que vc está pensando é: Gato.').
 mamiferos_menores() :- is_true('É famoso por participar em experimentos de laboratório'), is_true('Gosta de queijo'), write('O animal que vc está pensando é: Rato.').
 mamiferos_menores() :- is_true('Tem pata parecida com de patos'), is_true('Tem uma estrutura semelhante a um bico'), is_true('É um personagem do phineas e ferb'), write('O animal que vc está pensando é: Ornitorrinco.').
 mamiferos_menores() :- is_true('Voa'), is_true('Gosta de Sangue'), is_true('O Batman teve trauma desse animal quando criança'), write('O animal que vc está pensando é: Morcego.').
@@ -31,8 +31,8 @@ mamiferos_menores() :- write('Parabéns, você me venceu!! Até a próxima.'), h
 peixes() :- is_true('Vive nos mares'), peixes_salgado().
 peixes_salgado() :- is_true('Possui filamentos que lembram a crina de um cavalo'), is_true('O macho que faz a gestação'), is_true('Nadam com o corpo na vertical'),write('O animal que vc está pensando é: Cavalo-Marinho.').
 
-repteis() :- is_true('é pequeno'), is_true('anda na parede'), write('O animal que vc está pensando é: Lagartixa.').
-repteis() :- is_true('é grade'), is_true('vive nas margens de rios'), write('O animal que vc está pensando é: Crocodilo.').
+repteis() :- is_true('É pequeno'), is_true('Anda na parede'), write('O animal que vc está pensando é: Lagartixa.').
+repteis() :- is_true('É grade'), is_true('Vive nas margens de rios'), write('O animal que vc está pensando é: Crocodilo.').
 
 aves() :- is_true('Esse animal voa'), aves_voam().
 aves() :- is_true('O seu ovo é consumido com muita frequencia'),is_true('É conhecida por sua versão pintadinha fazer grande sucesso com o publico infantil'), write('O animal que vc está pensando é: Galinha').
