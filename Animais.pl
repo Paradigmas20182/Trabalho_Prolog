e0cafadea8889b9a306e5299c70f75139273a7b8
Lucas Siqueira <lucassiqzro@gmail.com>
2018-10-13 16:32:54 -0300

    Adicionando aves.
    Signed-off-by: Lucas Siqueira <lucassiqzro@gmail.com>
    Co-Authored-by: Lucas Pereira <lpalucas.10@gmail.com>
    Co-Authored-by: Bruno Dantas <oliveiradantas96@gmail.com>

--- Animais.pl
+++ Animais.pl
@@ -15,10 +15,17 @@ mamiferos_menores() :- is_true('Tem pata parecida com de patos'), is_true('Tem u
 mamiferos_menores() :- is_true('Voa'), is_true('Gosta de Sangue'), is_true('O Batman teve trauma desse animal quando criança'), write('O animal que vc está pensando é: Morcego.').
 
 
-mamiferos_menores() :- write('Parabéns, você me venceu!! Até a próxima.'), halt(0). 
+mamiferos_menores() :- write('Parabéns, você me venceu!! Até a próxima.'), halt(0).
 
 repteis() :- is_true('é pequeno'), is_true('anda na parede'), write('O animal que vc está pensando é: Lagartixa.').
 repteis() :- is_true('é grade'), is_true('vive nas margens de rios'), write('O animal que vc está pensando é: Crocodilo.').
 
-aves() :- is_true('se encontra em qualquer lugar'), is_true('faz pru'), write('O animal que vc está pensando é: Pombo.').
-aves() :- is_true('é personagem do filme "Rio"'), is_true('é azul'), write('O animal que vc está pensando é: Arara azul.').
+aves() :- is_true('Esse animal voa'), aves_voam().
+aves() :- is_true('O seu ovo é consumido com muita frequencia'),is_true('É conhecida por sua versão pintadinha fazer grande sucesso com o publico infantil'), write('O animal que vc está pensando é: Galinha').
+aves() :- is_true('Vive em ambiente frio'), is_true('Ele é o personagem principal do filme "happy feet"'), write('O animal que vc está pensando é: Pinguim').
+aves() :- is_true('É conhecido como a maior ave'), is_true('É famoso por seu ovo ser muito grande'), write('O animal que vc está pensando é: Avestruz').
+
+
+aves_voam() :- is_true('Se encontra em qualquer lugar'), is_true('Faz pru'), write('O animal que vc está pensando é: Pombo.').
+aves_voam() :- is_true('É personagem do filme "Rio"'), is_true('É azul'), write('O animal que vc está pensando é: Arara azul.').
+aves_voam() :- is_true('Existe um jogador de futsal com o mesmo nome deste animal'), write('O animal que vc está pensando é: Falcão').
